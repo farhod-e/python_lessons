a4f8b2a3902ad1ececd6e38e002fd62f0b2a38a6
Farhod <166046892+farhod-e@users.noreply.github.com>
2024-04-06 22:14:25 +0500

    Create lesson02.py

---
+++ lesson02.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 12:15:16 2024
+
+@author: Asus
+"""
+
+print("Hello world!")
+
+# amaliyot
+print('Assalomu aleykum')
+print("Hayrli tong!")
+print(2+2*4)
+print(19/5)
+print(2**4)
