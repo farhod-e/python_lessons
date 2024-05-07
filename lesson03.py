181fb812a6cf1b4f1b374a70970f6a4c895a2031
Farhod <166046892+farhod-e@users.noreply.github.com>
2024-05-07 18:24:09 +0500

    Add files via upload

---
+++ lesson03.py
@@ -0,0 +1,31 @@
+""" 03-lesson. print()
+Created on Mon Mar 25 12:15:16 2024
+@author: Farhod
+"""
+
+
+print(Good morning!)                    #invalid syntax
+print('Hello world')
+print('I buy laptop mark of "Dell"')
+print("I buy laptop mark of \"Dell\"")  #back slash
+print("""Odami ersang, demagil odami,
+Oniki, yo'q xalq g'amidin g'ami""")
+print("Odami ersang, demagil odami, \nOniki, yo'q xalq g'amidin g'ami")
+print('Odami ersang, demagil odami, \nOniki, yo\'q xalq g\'amidin g\'ami')
+
+print(2+4*2)
+print(20/5)
+print(16//4)
+print(10//3)
+
+print("To'qqizning kvadrati", 9**2, "ga teng")
+print('3x3=', 3*3)
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar xavas")
+
+# practices
+print("5 ning 4-darajasi", 5**4, "ga teng")
+print("22 ni 4 ga bo'lganda", 22%4, "qoldiq qoladi")
+print("Tomonlari 125 ga teng bo'lgan kvadratning yuzi", 125**2, "va perimetri", 125*4, "ga teng")
+print("Diametri 12 ga teng bo'lgan aylananing yuzi", (12/2)**2*3.14, "ga teng")
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2), "ga teng")
