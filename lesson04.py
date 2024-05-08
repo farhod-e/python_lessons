b2eab576516b7e6c63b3e47d9195b0b6324d90e3
Farhod <166046892+farhod-e@users.noreply.github.com>
2024-05-08 09:55:45 +0500

    Add files via upload

---
+++ lesson04.py
@@ -0,0 +1,33 @@
+""" 04-lesson. VARIABLES
+Created on 27.03.2024
+@author: Farhod
+"""
+
+
+name = "Maqsud"
+age = 22
+print(name)
+print(age)
+
+a = 6
+b = 7
+c = (a+b)**2
+print(c)
+
+
+full_name = 'Esanov Farhod'
+
+# practices
+
+start_program = 'Hello world!'
+print(start_program)
+
+letter = 'Bugungi kunga kelib python dasturlash tilini o\'rganishni boshladim'
+print(letter)
+letter = 29
+print(letter)
+
+radius = 25
+pi = 3.14159
+aylana_yuzi = pi*radius**2
+print("Radiusi", radius, "ga teng bo'lgan aylananing yuzi", aylana_yuzi, "ga teng")
